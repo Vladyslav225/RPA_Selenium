083de12214d48db7705c870e3a82d25fd371e111
Vlaadyslav225 <yvv.work@gmail.com>
2022-02-04 01:23:27 +0200

    .

--- new_robot/task.py
+++ new_robot/task.py
@@ -12,10 +12,17 @@ from re import search
 from RPA.Browser.Selenium import Selenium
 # from RPA.FileSystem import FileSystem
 # from RPA.Excel.Application import Application
+from RPA.PDF import PDF
 
 import time
+from openpyxl import Workbook
 
 browser = Selenium()
+
+
+list_name = []
+list_money = []
+
 
 # app = Application()
 
@@ -26,7 +33,7 @@ def open_the_website(url):
     
     # FileSystem().create_file("output/text.txt", text_1, overwrite=True)
 
-    #TODO find text all elements (name company, money company) and save to exel
+    #TODO Save to exel
 
 # Click element "DIVE IN"
 def open_list_agencies():
@@ -34,18 +41,14 @@ def open_list_agencies():
     
     browser.click_element("class:btn.btn-default.btn-lg-2x.trend_sans_oneregular")
 
-list_anme_money = []
 #Search block with name and money Agencies
 def search_block_agencies():
-    dict_name_money = {}
+    
 
     # pass
     block_agency = browser.find_elements("id:agency-tiles-widget")
     # print(len(block_agency))
 
-    # for item in block_agency:
-
-    #     item = block_agency
 
     for elements in block_agency:
         name_agencies = browser.find_elements('class:h4.w200', elements)
@@ -53,48 +56,36 @@ def search_block_agencies():
 
         money_agencies = browser.find_elements('class:h1.w900', elements)
         
-        for text1 in name_agencies:
-            text_name = browser.get_text(text1)
-            print(text_name)
-            # dict_name_money['Name agency'] = text_name
+        for name in name_agencies:
+            text_name = browser.get_text(name)
+            # print(text_name)
 
-        for text2 in money_agencies:
-            text_money = browser.get_text(text2)
-            print(text_money)
-            # dict_name_money['Money agency'] = text_money
+            list_name.append(text_name)
 
+        for money in money_agencies:
+            text_money = browser.get_text(money)
+            # print(text_money)
 
-        #     print(dict_name_money)
+            list_money.append(text_money)
 
-        
-
-            
+    return list_name, list_money
 
 
 
-        
+def save_to_xlsx(list_name_and_money):
+    wb = Workbook()
+    book = wb.active
 
-    # list_anme_money.append(dict_name_money)
+    for item in list_name_and_money:
 
-    # print(dict_name_money)
+        for i in item:
 
-    # return list_anme_money
+            book['A1'] = i
 
+            book.append([1, 2, 3])
 
-# Get element with name agencies
-def get_text_agencies(elements):
-    pass
+            wb.save('output/file.xlsx')
 
-
-
-def save_to_xlsx():
-    pass
-    # app.open_application()
-    # app.open_workbook('workbook.xlsx')
-    # app.set_active_worksheet(sheetname='new stuff')
-    # app.write_to_cells(row=1, column=1, value='new data')
-    # app.save_excel()
-    # app.quit_application()
 
 
 
@@ -104,14 +95,16 @@ def main():
         open_list_agencies()
         time.sleep(5)
 
-        search_block_agencies()
+        list_name_and_money = search_block_agencies()
+        # print(list_name_and_money)
+
         time.sleep(5)
-        print('done')
 
+        # get_text_agencies(elements=list_name_and_money)
 
-        # get_text_agencies(elements=search)
-
-        save_to_xlsx()
+        save_to_xlsx(list_name_and_money)
+        # print('done')
+        
     finally:
         browser.close_browser()
 
