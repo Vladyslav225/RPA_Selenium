94e7c865bd339a6540303441c460f5e53961cc0d
Vlaadyslav225 <yvv.work@gmail.com>
2022-02-04 19:03:55 +0200

    .

--- new_robot/task.py
+++ new_robot/task.py
@@ -2,7 +2,6 @@
 
 
 from RPA.Browser.Selenium import Selenium
-
 from RPA.PDF import PDF
 
 # I use openpyxl, because library "RPA.Excel.Application" work with Windows.
@@ -33,6 +32,7 @@ def open_list_agencies():
     except Exception as ex:
         print(ex)
 
+
 #Get text name and money Agencies
 def search_block_agencies():
     try:
@@ -42,43 +42,79 @@ def search_block_agencies():
         print(ex)
 
     for elements in block_agency:
-        name_agencies = browser.find_elements('class:h4.w200', elements)
+        url_agencies = browser.click_link('class:btn.btn-default.btn-sm', elements)
+        print(url_agencies)
 
-        money_agencies = browser.find_elements('class:h1.w900', elements)
+        # for url in url_agencies:
+        #     text_url = browser.get_text(url)
+        #     print(text_url)
+            # list_name.append(text_url)
 
-        for name in name_agencies:
-            text_name = browser.get_text(name)
-            list_name.append(text_name)
 
-        for money in money_agencies:
-            text_money = browser.get_text(money)
-            list_money.append(text_money)
+    #     name_agencies = browser.find_elements('class:h4.w200', elements)
 
-    return list_name, list_money
+    #     money_agencies = browser.find_elements('class:h1.w900', elements)
+
+    #     for name in name_agencies:
+    #         text_name = browser.get_text(name)
+    #         list_name.append(text_name)
+
+    #     for money in money_agencies:
+    #         text_money = browser.get_text(money)
+    #         list_money.append(text_money)
+
+    # return list_name, list_money
 
 # Saving to Exel information about Agencies
-def save_to_xlsx(filename):
+# def save_to_xlsx(filename):
 
-    wb = Workbook()
-    book = wb.active
-    book.title= 'Agencies'
+#     wb = Workbook()
+#     book = wb.active
+#     book.title= 'Agencies'
     
-    book['A1'] = 'Name companies'
-    book['B1'] = 'Money companies'
+#     book['A1'] = 'Name companies'
+#     book['B1'] = 'Money companies'
 
-    r = 2
-    for name in list_name:
-        book.cell(row=r, column=1).value = name
-        r += 1
+#     r = 2
+#     for name in list_name:
+#         book.cell(row=r, column=1).value = name
+#         r += 1
 
-    r = 2
-    for money in list_money:
-        book.cell(row=r, column=2).value = money
-        r += 1
+#     r = 2
+#     for money in list_money:
+#         book.cell(row=r, column=2).value = money
+#         r += 1
 
-    wb.save(filename=filename)
-    wb.close()
+#     wb.save(filename=filename)
+#     wb.close()
 
+
+# def url_agencies():
+#     url = 
+    
+
+def check_agensy(url):
+    pass
+    # url_agency = url + "/drupal/summary/005"
+    # browser.open_available_browser(url + "drupal/summary/005")
+    # time.sleep(3)
+
+    # element = browser.find_element('class:h1.w200.martop-0.marbottom-0')
+
+    # b = browser.get_text(element)
+    # print(b)
+
+    # print('done')
+
+
+
+
+# def check_agensy(url):
+#     agency = url + 'National Science Foundation'
+#     time.sleep(5)
+#     # {'agency'} = browser.find_element('value: National Science Foundation')
+#     # print(n)
+#     print('done')
 
 
 
@@ -89,7 +125,11 @@ def main():
         time.sleep(5)
         search_block_agencies()
         time.sleep(5)
-        save_to_xlsx('output/file.xlsx')
+        # save_to_xlsx('output/file.xlsx')
+
+        # url_agencies()
+
+        check_agensy("https://itdashboard.gov/")
         
     finally:
         browser.close_browser()
