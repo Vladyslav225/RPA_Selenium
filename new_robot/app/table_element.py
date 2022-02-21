63f91b1cc078829240e544f6ee4509f766559f73
Vlaadyslav225 <yvv.work@gmail.com>
2022-02-22 00:13:39 +0200

    .

--- new_robot/app/table_element.py
+++ new_robot/app/table_element.py
@@ -5,9 +5,18 @@ from RPA.Browser.Selenium import Selenium
 
 browser = Selenium()
 
+# # list_uii = []
+# # Elements and Text with class "ODD"
+# # ELEMENTS
+# def odd(all_elements):
+#     list_uii = []
+#     for element in all_elements:
+#         all_elements = browser.find_elements('xpath:/td[@class="left sorting_2"]', element)
+#         for text in all_elements:
+#             text_uii_elements = browser.get_text(text)
+#             list_uii.append(text_uii_elements)
 
-# Elements and Text with class "ODD"
+#         print(list_uii)
 
-# ELEMENTS
-def odd(all_elements):
-    print(all_elements)
+
+#     # return text_uii_elements
--- new_robot/task.py
+++ new_robot/task.py
@@ -13,14 +13,20 @@ import time
 
 # app
 from app.finder_agency import get_element
-from app.table_element import odd
+# from app.table_element import elements_table_body
 
 browser = Selenium()
 
+pdf = PDF()
 
 # List Name and Spanding Agencies from Start page
 name_spending_agency = []
 
+# List elements from table Agency
+list_text_table = []
+
+# List URL from table Agency
+list_url_table = []
 
 
 
@@ -136,17 +142,131 @@ def block_body_table(body_table):
 
 
 # Getting table body text
-def elements_table_body():
+def elements_table_body(elements):
+    # list_uii = []
+    # list_bureau = []
+    # list_investment = []
+    # list_spending = []
+    # list_type = []
+    # list_rating = []
+    # list_projects = []
+
+
+    # UII
     try:
-        all_elements = browser.find_elements('xpath://tr')
+        uii_elements = browser.find_elements('xpath://tr/td[1][@class="left sorting_2"]', elements)
 
     except Exception as ex:
         print(ex)
 
+    for url in uii_elements:
+        finder_url = browser.find_elements('tag:a', url)
 
-    # Elements with class - odd
-    odd(all_elements)
+        for item in finder_url:
+            if item ==  None:
+                continue
+
+            list_url_table.append(item)
+
+    # for text_uii in uii_elements:
+    #     list_uii.append(browser.get_text(text_uii))
+
+
+    # # Bureau
+    # try:
+    #     bureau_elements = browser.find_elements('xpath://tr/td[2][@class=" left select-filter"]', elements)
+
+    # except Exception as ex:
+    #     print(ex)
+
+    # for text_bureau in bureau_elements:
+    #     list_bureau.append(browser.get_text(text_bureau))
+
+
+    # # Investment Title
+    # try:
+    #     investment_elements = browser.find_elements('xpath://tr/td[3][@class=" left"]', elements)
+
+    # except Exception as ex:
+    #     print(ex)
+
+    # for text_investment in investment_elements:
+    #     list_investment.append(browser.get_text(text_investment))
+
+
+    # # Total Spending
+    # try:
+    #     spending_elements = browser.find_elements('xpath://tr/td[4][@class=" right"]', elements)
+
+    # except Exception as ex:
+    #     print(ex)
+
+    # for text_spending in spending_elements:
+    #     list_spending.append(browser.get_text(text_spending))
+
+
+    # # Type
+    # try:
+    #     type_elements = browser.find_elements('xpath://tr/td[5][@class=" left select-filter"]', elements)
+
+    # except Exception as ex:
+    #     print(ex)
+
+    # for text_type in type_elements:
+    #     list_type.append(browser.get_text(text_type))
+
+
+    # # CIO Rating
+    # try:
+    #     rating_elements = browser.find_elements('xpath://tr/td[6][@class=" center"]', elements)
+
+    # except Exception as ex:
+    #     print(ex)
+
+    # for text_rating in rating_elements:
+    #     list_rating.append(browser.get_text(text_rating))
+
     
+    # # Projects
+    # try:
+    #     projects_elements = browser.find_elements('xpath://tr/td[7][@class=" center"]', elements)
+
+    # except Exception as ex:
+    #     print(ex)
+
+    # for text_projects in projects_elements:
+    #     list_projects.append(browser.get_text(text_projects))
+
+
+    # list_text_table.append(
+    #     {
+    #         'UII': list_uii,
+    #         'Bureau': list_bureau,
+    #         'Investment Title': list_investment,
+    #         'Total Spending': list_spending,
+    #         'Type': list_type,
+    #         'CIO Rating': list_rating,
+    #         'Projects': list_projects
+    #     }
+    # )
+
+    # return list_text_table
+
+
+# Open pages from table
+def open_url_table(file_pdf):
+    open_page = browser.click_element(list_url_table[0])
+    browser.set_browser_implicit_wait(10)
+
+    # Download Business Case PDF
+    element_case = browser.find_element('id:business-case-pdf')
+
+    browser.click_element(element_case)
+    time.sleep(20)
+
+    print('DONE, open_url_table')
+
+
 
 def main():
     try:
@@ -181,10 +301,13 @@ def main():
         time.sleep(10)
 
     # Getting block body table elements
-        block_body_table(body_table=block_table_agency)
+        all_elements = block_body_table(body_table=block_table_agency)
 
     # Getting table body text
-        elements_table_body()
+        elements_table_body(elements=all_elements)
+
+    # Open pages from table
+        open_url_table('new_robot/output')
 
         print('done')
         
